9641ef653217cf812941993727584cda8701b2a0
Gurumoorthy Santhakumar <gsanthak@qti.qualcomm.com>
2025-04-16 14:43:37 +0530

    config: Enable Ethernet support
    Supported PHYs:
    - QCA81xx
    - QCA8075
    - QCA8x8x
    - QCA8081
    - SFP-PHY10G
    
    Change-Id: Icda4a02fe53049253811c293eed09e2ac8642bf5
    Signed-off-by: Ram Kumar D <ramd@qti.qualcomm.com>
    Signed-off-by: Vandhiadevan Karunamoorthy <vkarunam@qti.qualcomm.com>
    Signed-off-by: Gurumoorthy Santhakumar <gsanthak@qti.qualcomm.com>

--- include/configs/ipq9574.h
+++ include/configs/ipq9574.h
@@ -8,6 +8,7 @@
 
 #ifndef __ASSEMBLY__
 #include <linux/types.h>
+#include <linux/sizes.h>
 extern uint32_t g_board_machid;
 #endif
 
@@ -16,9 +17,23 @@ extern uint32_t g_board_machid;
 		(CONFIG_TEXT_BASE - CONFIG_SYS_MALLOC_LEN -\
 			CONFIG_ENV_SIZE - GENERATED_GBL_DATA_SIZE)
 
-#define CONFIG_MACH_TYPE		(g_board_machid)
-#define CFG_SYS_SDRAM_BASE		0x40000000
-#define KERNEL_START_ADDR		CFG_SYS_SDRAM_BASE
-#define CFG_ROOTFS_LOAD_ADDR		(CFG_SYS_SDRAM_BASE + (32 << 20))
-#define FDT_HIGH			0x48500000
+#define CONFIG_MACH_TYPE			(g_board_machid)
+#define CFG_SYS_SDRAM_BASE0_ADDR		0x40000000
+#define CFG_SYS_SDRAM_BASE0_SIZE		0xC0000000
+
+#define CFG_SYS_SDRAM_BASE			CFG_SYS_SDRAM_BASE0_ADDR
+#define KERNEL_START_ADDR			CFG_SYS_SDRAM_BASE
+#define BOOT_PARAMS_ADDR			(CFG_SYS_SDRAM_BASE + 0x100)
+#define CFG_ROOTFS_LOAD_ADDR			(CFG_SYS_SDRAM_BASE + (32 << 20))
+#define FDT_HIGH				0x48500000
+
+#define PHY_ANEG_TIMEOUT			100
+
+#define IPQ9574_UBOOT_END_ADDRESS		CONFIG_TEXT_BASE + \
+							CONFIG_TEXT_SIZE
+
+#define NONCACHED_MEM_REGION_ADDR		((IPQ9574_UBOOT_END_ADDRESS + \
+						SZ_1M - 1) & ~(SZ_1M - 1))
+#define NONCACHED_MEM_REGION_SIZE		SZ_1M
+
 #endif /* _IPQ9574_H_ */
