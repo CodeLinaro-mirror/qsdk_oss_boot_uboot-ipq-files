8634943504434f6b146761375394ce7ccc9022ac
Vandhiadevan Karunamoorthy <vkarunam@qti.qualcomm.com>
2025-06-04 09:46:32 +0530

    mtd: Reset usecount to 0 for clean device removal
    Ensure the filesystem mounted on the MTD partition is
    unmounted before removing the MTD device.
    
    This helps reset the usecount to 0, allowing for a
    clean and safe removal process.
    
    Change-Id: Ib28e028770b8833bf564979d6fc973ad7ace1d8e
    Signed-off-by: Vandhiadevan Karunamoorthy <vkarunam@qti.qualcomm.com>

--- drivers/mtd/nand/raw/qcom_nand.c
+++ drivers/mtd/nand/raw/qcom_nand.c
@@ -4346,6 +4346,7 @@ U_BOOT_DRIVER(qti_nand) = {
 int qti_nand_deinit(struct udevice *device)
 {
 	int ret = 0;
+	struct mtd_info *slave, *next;
 	struct qcom_nand_controller *nandc = NULL;
 	struct mtd_info *mtd = get_nand_dev_by_index(dev_seq(device));
 	if(!mtd) {
@@ -4358,6 +4359,15 @@ int qti_nand_deinit(struct udevice *device)
 		printf("%s: nand controller not available\n", __func__);
 		return -ENOMEM;
 	}
+	/*
+	 * Unmount the filesystem that was mounted on the MTD partition
+	 * before removing the MTD device.
+	 * For example, this applies to filesystems like rootfs.
+	 */
+	list_for_each_entry_safe(slave, next, &mtd->partitions, node) {
+		put_mtd_device(slave);
+	}
+
 
 	ret = del_mtd_device(mtd);
 	if (ret < 0)
