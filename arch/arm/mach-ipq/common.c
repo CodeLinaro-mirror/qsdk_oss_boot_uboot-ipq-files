0b1f22bda5a63cd167ac161a345224eb316d7a33
Gurumoorthy Santhakumar <gsanthak@qti.qualcomm.com>
2025-10-10 22:45:17 +0530

    arch: ipq: common: Fix ptable memory allocation
    Update the ptable address in bdinfo during MIBIB initialization
    
    Change-Id: I434a5419c48641991630747d4d34b4197e90cb8a
    Signed-off-by: Gurumoorthy Santhakumar <gsanthak@qti.qualcomm.com>

--- arch/arm/mach-ipq/common.c
+++ arch/arm/mach-ipq/common.c
@@ -1549,6 +1549,10 @@ int mibib_ptable_init(unsigned int *addr)
 {
 	struct smem_ptable *mib_ptable;
 	struct smem_ptable *ptable = ipq_get_part_table();
+	struct ipq_board_info *bdinfo = ipq_get_bdinfo();
+
+	if (!bdinfo)
+		return -ENXIO;
 
 	mib_ptable = (struct smem_ptable *) addr;
 	if (mib_ptable->magic[0] != _SMEM_PTABLE_MAGIC_1 ||
@@ -1562,6 +1566,7 @@ int mibib_ptable_init(unsigned int *addr)
 		ptable = malloc(sizeof(struct smem_ptable));
 		if (!ptable)
 			return -ENOMEM;
+		bdinfo->ptable = ptable;
 	}
 
 	memcpy(ptable, addr, sizeof(struct smem_ptable));
