998beddb0f184bf43ca2f51eea4fc1be45cf7de2
Timple Raj M <timple@qti.qualcomm.com>
2025-12-23 12:55:46 +0530

    arm: dts: ipq5210: Rename chipset reference in file
    Update the file to replace the chipset reference
    from ipq5200 to ipq5210 for consistency.
    
    Change-Id: I9892fca8662da1c9ffce6bb93f8438fb2c95b947
    Signed-off-by: Timple Raj M <timple@qti.qualcomm.com>

--- arch/arm/mach-ipq/include/mach/ipq5210.h
+++ arch/arm/mach-ipq/include/mach/ipq5210.h
@@ -3,8 +3,8 @@
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
-#ifndef __IPQ5200_H__
-#define __IPQ5200_H__
+#ifndef __IPQ5210_H__
+#define __IPQ5210_H__
 
 #define TCSR_BOOT_MISC_REG			((u32 *)0x195C100)
 
--- drivers/clk/qcom/clock-ipq5210.c
+++ drivers/clk/qcom/clock-ipq5210.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Clock drivers for Qualcomm ipq5200
+ * Clock drivers for Qualcomm IPQ5210
  *
  * (C) Copyright 2024 Linaro Ltd.
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
@@ -14,8 +14,8 @@
 #include <linux/bug.h>
 #include <linux/bitops.h>
 #include <dm/device-internal.h>
-#include <asm/arch/dt-bindings/clock/qcom,ipq5200-gcc.h>
-#include <asm/arch/dt-bindings/reset/qcom,ipq5200-gcc.h>
+#include <asm/arch/dt-bindings/clock/qcom,ipq5210-gcc.h>
+#include <asm/arch/dt-bindings/reset/qcom,ipq5210-gcc.h>
 #include "clock-qcom.h"
 
 #define	GCC_QUPV3_UART1_CMD_RCGR		0x5004
@@ -79,7 +79,7 @@ ulong msm_get_rate(struct clk *clk)
 	return (ulong)clk->rate;
 }
 
-static ulong ipq5200_set_rate(struct clk *clk, ulong rate)
+static ulong ipq5210_set_rate(struct clk *clk, ulong rate)
 {
 	struct msm_clk_priv *priv = dev_get_priv(clk->dev);
 	int src, div = 0;
@@ -203,7 +203,7 @@ static ulong ipq5200_set_rate(struct clk *clk, ulong rate)
 	return rate;
 }
 
-static const struct gate_clk ipq5200_clks[] = {
+static const struct gate_clk ipq5210_clks[] = {
 	GATE_CLK(GCC_QUPV3_UART1_CLK,		0x05020, 0x00000001),
 	GATE_CLK(GCC_SDCC1_AHB_CLK,		0x3303C, 0x00000001),
 	GATE_CLK(GCC_SDCC1_APPS_CLK,		0x3302C, 0x00000001),
@@ -235,7 +235,7 @@ static const struct gate_clk ipq5200_clks[] = {
 	GATE_CLK(GCC_CNOC_PCIE1_2LANE_S_CLK,	0x3108C,  0x00000001),
 };
 
-static int ipq5200_enable(struct clk *clk)
+static int ipq5210_enable(struct clk *clk)
 {
 	struct msm_clk_priv *priv = dev_get_priv(clk->dev);
 
@@ -244,14 +244,14 @@ static int ipq5200_enable(struct clk *clk)
 		return 0;
 	}
 
-	debug("%s: clk %s\n", __func__, ipq5200_clks[clk->id].name);
+	debug("%s: clk %s\n", __func__, ipq5210_clks[clk->id].name);
 
 	qcom_gate_clk_en(priv, clk->id);
 
 	return 0;
 }
 
-static const struct qcom_reset_map ipq5200_gcc_resets[] = {
+static const struct qcom_reset_map ipq5210_gcc_resets[] = {
 	[GCC_SDCC_BCR]			= {0x33000, 0},
 	[GCC_USB0_PHY_BCR]		= {0x2C06C, 0},
 	[GCC_USB3PHY_0_PHY_BCR]		= {0x2C070, 0},
@@ -279,27 +279,27 @@ static const struct qcom_reset_map ipq5200_gcc_resets[] = {
 	[GCC_PCIE0_AUX_ARES]		= {0x28070, 2},
 };
 
-static struct msm_clk_data ipq5200_gcc_data = {
-	.resets = ipq5200_gcc_resets,
-	.num_resets = ARRAY_SIZE(ipq5200_gcc_resets),
-	.clks = ipq5200_clks,
-	.num_clks = ARRAY_SIZE(ipq5200_clks),
-	.enable = ipq5200_enable,
-	.set_rate = ipq5200_set_rate,
+static struct msm_clk_data ipq5210_gcc_data = {
+	.resets = ipq5210_gcc_resets,
+	.num_resets = ARRAY_SIZE(ipq5210_gcc_resets),
+	.clks = ipq5210_clks,
+	.num_clks = ARRAY_SIZE(ipq5210_clks),
+	.enable = ipq5210_enable,
+	.set_rate = ipq5210_set_rate,
 };
 
-static const struct udevice_id gcc_ipq5200_of_match[] = {
+static const struct udevice_id gcc_ipq5210_of_match[] = {
 	{
-		.compatible = "qcom,ipq5200-gcc",
-		.data = (ulong)&ipq5200_gcc_data,
+		.compatible = "qcom,ipq5210-gcc",
+		.data = (ulong)&ipq5210_gcc_data,
 	},
 	{ }
 };
 
-U_BOOT_DRIVER(gcc_ipq5200) = {
-	.name		= "gcc_ipq5200",
+U_BOOT_DRIVER(gcc_ipq5210) = {
+	.name		= "gcc_ipq5210",
 	.id		= UCLASS_NOP,
-	.of_match	= gcc_ipq5200_of_match,
+	.of_match	= gcc_ipq5210_of_match,
 	.bind		= qcom_cc_bind,
 	.flags		= DM_FLAG_PRE_RELOC | DM_FLAG_DEFAULT_PD_CTRL_OFF,
 };
