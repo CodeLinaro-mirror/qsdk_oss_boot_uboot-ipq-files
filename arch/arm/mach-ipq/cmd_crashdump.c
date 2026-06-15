CodeLinaro-mirror/qsdk_oss_boot_uboot-ipq-files

commit 50a771621c5b2702e2f2b2eb9c0abac985287f05

crashdump: Add support for USB directory-based dump
Previously, all dumps were collected in the USB root directory,
This update enables storing dumps in specific folder on USB,
the folder name is determined by "dumpdir" env variable
The feature is supported for full dumps.

Change-Id: I7b63c245f20b68f2634ee14d70c3ba7e3e2a32fb
Signed-off-by: Badhrinath S <badhrina@qti.qualcomm.com>

arch/arm/mach-ipq/cmd_crashdump.c
@@ -1602,6 +1602,7 @@ static int prepare_crashdump_level_table(crashdump_config_t *dump_config,
 	crashdump_infos_int_t dump_entry;
 	struct crashdump_infos *dump_infos = dump_config->dump_infos;
 	char dump_name_prefix[DUMP_NAME_STR_MAX_LEN] = { 0 };
+	char *dump_dir = env_get("dumpdir");
 	uint64_t split_bin_sz = 0;
 	uint8_t file_no = 0;
 #if (CONFIG_NR_DRAM_BANKS > 1)
@@ -1623,12 +1624,48 @@ static int prepare_crashdump_level_table(crashdump_config_t *dump_config,
 
 		file_no = 0;
 		memset(&dump_entry, 0, sizeof(crashdump_infos_int_t));
-		memcpy(&dump_name_prefix, dump_infos[i].name,
-					(strlen(dump_infos[i].name) - 4));
-		dump_name_prefix[(strlen(dump_infos[i].name) - 4)] = '\0';
+		/*
+		 * add dump path to dump info table if dumpdir env exists
+		 */
+		if (dump_dir && dump_config->dump_to == DUMP_TO_USB) {
+			const char *name = dump_infos[i].name;
+			size_t name_len = strlen(name);
+			size_t dir_len = strlen(dump_dir);
 
-		strlcpy(dump_entry.name, dump_infos[i].name,
+			if(dir_len + name_len + 1 >= DUMP_NAME_STR_MAX_LEN) {
+				printf("dump path info length is exceeded maximum length allowed\n");
+				goto usb_default_dump;
+			}
+
+			if (name_len > 4 && strcmp(name + name_len - 4,
+						   ".BIN") == 0) {
+				char name_without_ext[DUMP_NAME_STR_MAX_LEN];
+
+				strlcpy(name_without_ext, name, name_len - 3);
+				name_without_ext[name_len - 3] = '\0';
+
+				snprintf(dump_name_prefix,
+					 sizeof(dump_name_prefix), "%s/%s",
+					 dump_dir, name_without_ext);
+				snprintf(dump_entry.name,
+					 sizeof(dump_entry.name), "%s/%s",
+					 dump_dir, name);
+			} else {
+				snprintf(dump_name_prefix,
+					 sizeof(dump_name_prefix),
+					 "%s/%s", dump_dir, name);
+				snprintf(dump_entry.name,
+					 sizeof(dump_entry.name),
+					 "%s/%s", dump_dir, name);
+			}
+		} else {
+usb_default_dump:
+			memcpy(&dump_name_prefix, dump_infos[i].name,
+			       (strlen(dump_infos[i].name) - 4));
+			dump_name_prefix[(strlen(dump_infos[i].name) - 4)] = '\0';
+			strlcpy(dump_entry.name, dump_infos[i].name,
 				DUMP_NAME_STR_MAX_LEN);
+		}
 		dump_entry.start_addr = dump_infos[i].start_addr;
 		dump_entry.size = dump_infos[i].size;
 		dump_entry.is_aligned_access = dump_infos[i].is_aligned_access;
@@ -2718,6 +2755,24 @@ void ipq_do_dump_data(crashdump_config_t *dump_config)
 		}
 	}
 #endif /* CONFIG_IPQ_CRASHDUMP_TO_FLASH */
+
+#ifdef CONFIG_IPQ_CRASHDUMP_TO_USB
+	char *dump_dir = env_get("dumpdir");
+
+	if (dump_dir && dump_config->dump_to == DUMP_TO_USB) {
+		char runcmd[50] = {0};
+
+		printf("Creating dir %s\n", dump_dir);
+		snprintf(runcmd, sizeof(runcmd),
+			 "fatmkdir usb %x:%x %s",
+			 iface_cfg->usb_dev_idx,
+			 iface_cfg->usb_part_idx,
+			 dump_dir);
+		if (run_command(runcmd, 0) != CMD_RET_SUCCESS) {
+			printf("Warning: Failed to create directory %s\n", dump_dir);
+		}
+	}
+#endif /* CONFIG_IPQ_CRASHDUMP_TO_USB */
 
 	list_for_each_entry(dump_entry, &actual_dumps_list, list) {
 		printf("Processing %s:\n", dump_entry->name);

arch/arm/mach-ipq/include/mach/ipq.h
@@ -89,7 +89,7 @@
 #endif
 #define CFG_IPQ_NAND_PART	       BIT(IPQ_NAND_FLASH_VALID_BIT)
 
-#define DUMP_NAME_STR_MAX_LEN		20
+#define DUMP_NAME_STR_MAX_LEN		50
 #define DUMP2MEM_MAGIC1_COOKIE		0x4D494E49
 #define DUMP2MEM_MAGIC2_COOKIE		0x44554D50
 
@@ -138,7 +138,7 @@
 #define reset()				do_reset(NULL, 0, 0, NULL)
 
 #ifndef DUMP_NAME_STR_MAX_LEN
-#define DUMP_NAME_STR_MAX_LEN		20
+#define DUMP_NAME_STR_MAX_LEN		50
 #endif
 
 /*

commit 8d256d9754ff687f94813677478d8f2e65823bce

crashdump: fix memdump_list_info_t struct size regression
Commit 50a77162 ("crashdump: Add support for USB directory-based
dump") increased DUMP_NAME_STR_MAX_LEN from 20 to 50 to support
USB directory paths (e.g., "dumpdir/FILENAME.BIN"). This changed
the memdump_list_info_t binary struct size from 36 to 72 bytes
due to C alignment padding, breaking the dump-to-memory/nvmem
parser (ramparse.py) with MemoryError.

Separate the concerns by introducing DUMP_PATH_STR_MAX_LEN = 50
for USB/EMMC directory path handling, while reverting
DUMP_NAME_STR_MAX_LEN to 20 to preserve the binary format of
memdump_list_info_t.

Add __attribute__((packed)) to memdump_list_info_t to ensure the
struct is exactly 36 bytes (name[20] + offset[8] + size[8]),
eliminating the 4-byte C alignment padding and matching the
parser's expected format.

Update crashdump_infos_int_t and local path variables to use
DUMP_PATH_STR_MAX_LEN. The USB directory dump feature continues
to work correctly. All other dump modes are unaffected.

Fixes: 50a77162 ("crashdump: Add support for USB directory-based dump")
Change-Id: Idd2923af5e70246d7b5dfb479549d10112b15f25
Signed-off-by: Gurumoorthy Santhakumar <gsanthak@qti.qualcomm.com>

arch/arm/mach-ipq/cmd_crashdump.c
@@ -64,7 +64,7 @@
 #if defined(CONFIG_IPQ_CRASHDUMP_TO_MEMORY) || \
 	defined(CONFIG_IPQ_CRASHDUMP_TO_NVMEMORY)
 
-typedef struct {
+typedef struct __attribute__((packed)) {
 	char name[DUMP_NAME_STR_MAX_LEN];
 	uint64_t offset;
 	uint64_t size;
@@ -208,7 +208,7 @@ static u8 g_minidump_value;
 #endif /* CONFIG_IPQ_MINIDUMP */
 
 typedef struct {
-	char name[DUMP_NAME_STR_MAX_LEN];
+	char name[DUMP_PATH_STR_MAX_LEN];
 	uint64_t start_addr;
 	uint64_t size;
 	uint8_t is_aligned_access:1;
@@ -1567,7 +1567,7 @@ static int verify_crashdump_iface(crashdump_config_t * dump_config)
  */
 static int split_bin_dump(crashdump_config_t *dump_config,
 		crashdump_infos_int_t *dump_entry, uint64_t split_size,
-		char dump_name_prefix[DUMP_NAME_STR_MAX_LEN],
+		char dump_name_prefix[DUMP_PATH_STR_MAX_LEN],
 		uint8_t file_no_start)
 {
 	int ret = 0;
@@ -1613,7 +1613,7 @@ static int prepare_crashdump_level_table(crashdump_config_t *dump_config,
 	int i, ret = 0;
 	crashdump_infos_int_t dump_entry;
 	struct crashdump_infos *dump_infos = dump_config->dump_infos;
-	char dump_name_prefix[DUMP_NAME_STR_MAX_LEN] = { 0 };
+	char dump_name_prefix[DUMP_PATH_STR_MAX_LEN] = { 0 };
 	char *dump_dir = env_get("dumpdir");
 	uint64_t split_bin_sz = 0;
 	uint8_t file_no = 0;
@@ -1644,14 +1644,14 @@ static int prepare_crashdump_level_table(crashdump_config_t *dump_config,
 			size_t name_len = strlen(name);
 			size_t dir_len = strlen(dump_dir);
 
-			if(dir_len + name_len + 1 >= DUMP_NAME_STR_MAX_LEN) {
+			if(dir_len + name_len + 1 >= DUMP_PATH_STR_MAX_LEN) {
 				printf("dump path info length is exceeded maximum length allowed\n");
 				goto usb_default_dump;
 			}
 
 			if (name_len > 4 && strcmp(name + name_len - 4,
 						   ".BIN") == 0) {
-				char name_without_ext[DUMP_NAME_STR_MAX_LEN];
+				char name_without_ext[DUMP_PATH_STR_MAX_LEN];
 
 				strlcpy(name_without_ext, name, name_len - 3);
 				name_without_ext[name_len - 3] = '\0';
@@ -3010,8 +3010,8 @@ static int dump_to_dst(crashdump_config_t *dump_config,
 	case DUMP_TO_EMMC:
 		loff_t len;
 		int ret;
-		char abs_file_path[DUMP_NAME_STR_MAX_LEN+1];
-		snprintf(abs_file_path, DUMP_NAME_STR_MAX_LEN+1, "/%s",
+		char abs_file_path[DUMP_PATH_STR_MAX_LEN+1];
+		snprintf(abs_file_path, DUMP_PATH_STR_MAX_LEN+1, "/%s",
 				dump_entry->name);
 
 		if (fs_set_blk_dev("mmc", iface_cfg->emmc_part_dev,

arch/arm/mach-ipq/include/mach/ipq.h
@@ -89,7 +89,8 @@
 #endif
 #define CFG_IPQ_NAND_PART	       BIT(IPQ_NAND_FLASH_VALID_BIT)
 
-#define DUMP_NAME_STR_MAX_LEN		50
+#define DUMP_NAME_STR_MAX_LEN		20
+#define DUMP_PATH_STR_MAX_LEN		50
 #define DUMP2MEM_MAGIC1_COOKIE		0x4D494E49
 #define DUMP2MEM_MAGIC2_COOKIE		0x44554D50
 
@@ -136,10 +137,6 @@
 #endif
 
 #define reset()				do_reset(NULL, 0, 0, NULL)
-
-#ifndef DUMP_NAME_STR_MAX_LEN
-#define DUMP_NAME_STR_MAX_LEN		50
-#endif
 
 /*
  * QCN9224 fusing